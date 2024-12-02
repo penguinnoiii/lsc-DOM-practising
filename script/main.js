825bfe9f23fa3aa4f544603f44e8d89df0127880
penguinnoiii <Pornnapatpoonpolsub@gmail.com>
2024-12-02 09:22:01 +0700

    New Commit

--- script/main.js
+++ script/main.js
@@ -1,1 +1,22 @@
-// Start here
+const button = document.getElementById("color-btn")
+
+button.addEventListener("click", () => {
+    const colorArray = ["#FF6633", "#FFB399", "#FF33FF", "#FFFF99", "#00B3E6"];
+    const i = Math.floor(Math.random() * colorArray.length)
+
+    const rainbow = document.getElementById("rainbow")
+    rainbow.style.backgroundColor = colorArray[i]
+})
+
+const addButton = document.getElementById("add-btn")
+
+addButton.addEventListener("click", () => {
+    const input = document.getElementById("item-input")
+    const list = document.getElementById("listItem").firstElementChild
+    const li = document.createElement("li")
+
+    li.textContent = input.value
+    list.appendChild(li)
+    console.log(list)
+})
+
